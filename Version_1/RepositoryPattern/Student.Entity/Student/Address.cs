dcad03aa0207d9654ebbd043606ad428e923bca3
DrMadWill <nofelsalahov9@gmail.com>
2022-06-22 18:40:22 +0400

    main: Api Project,Entity Model Create
    1) Api Project Create;2)Select Database Structur;3)Create Entity

---
+++ Version_1/RepositoryPattern/Student.Entity/Student/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Entity.Student
+{
+    public class Address
+    {
+        public Guardian Guardian { get; set; }
+        [Key, ForeignKey("Guardian")]
+        public int Id { get; set; }
+        [Required]
+        public string Adress { get; set; }
+        public string Adress2 { get; set; }
+
+        [StringLength(10)]
+        public int PostCode { get; set; }
+
+    }
+}
---
+++ Version_1/RepositoryPattern/Student.Entity/Student/Family.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Entity.Student
+{
+    public class Family
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(255,MinimumLength =3)]
+        [Required]
+        public string Name { get; set; }
+
+        public IList<Student> Students { get; set; }
+        public IList<Guardian> Guardians { get; set; }
+
+    }
+}
